fbda5a8efb9a8dc002b85cfc12ea1a055cbd8fe6
Guilherme Oliveira <guilhermeoc.developer@gmail.com>
2023-04-07 18:17:29 -0300

    ADD: Adicionando a interface

---
+++ ECommerceApi/Repositories/IUserRepository.cs
@@ -0,0 +1,24 @@
+﻿using ECommerceModels;
+
+namespace ECommerceApi.Repositories
+{
+    public interface IUserRepository
+    {
+        // Usando o padrão Repository
+
+        // Realizando o CRUD
+
+
+        public List<User> Get();
+
+        public User Get(int id);
+
+        public void Add(User user);
+
+        public void Update(User user);
+
+        public void Delete(int id);
+
+
+    }
+}
